e56df26b55b3520ad155de22b679e07a2d6d5988
Shashank Suman <shashanksuman@Shashanks-MacBook-Air-2.local>
2025-06-12 10:18:23 +0530

    initial commit

---
+++ concurrency/mutex/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var msg string
+
+var wg sync.WaitGroup
+
+func updateMessage(s string) {
+	defer wg.Done()
+
+	msg = s
+}
+
+func main() {
+	msg = "Hello, World!"
+
+	wg.Add(2)
+	go updateMessage("Hello, Universe!")
+	go updateMessage("Hello, cosmos!")
+	wg.Wait()
+
+	fmt.Println(msg)
+}
+
+// func updateMessage(s string, m *sync.Mutex) {
+// 	defer wg.Done()
+
+// 	m.Lock()
+// 	msg = s
+// 	m.Unlock()
+// }
+
+// func main() {
+// 	msg = "Hello, World!"
+
+// 	var mutex sync.Mutex
+
+// 	wg.Add(2)
+// 	go updateMessage("Hello, Universe!", &mutex)
+// 	go updateMessage("Hello, cosmos!", &mutex)
+// 	wg.Wait()
+
+// 	fmt.Println(msg)
+// }
